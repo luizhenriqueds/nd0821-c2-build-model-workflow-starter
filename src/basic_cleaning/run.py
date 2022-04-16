d804dab0f9ee0da68260f86f2b12da28549f10f6
Luiz Henrique <luizhsda@gmail.com>
2022-04-16 15:01:13 -0300

    fixed proper boundaries failed test

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -26,6 +26,9 @@ def _preprocess_data(df, min_price, max_price) -> pd.DataFrame:
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df['last_review'] = pd.to_datetime(df['last_review'])
     return df
 
